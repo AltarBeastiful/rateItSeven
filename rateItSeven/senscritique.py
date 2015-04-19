7b25c3babcde52ff81b3c74171357a2c5861ae35
Rémi Benoit <r3m1.benoit@gmail.com>
2015-04-19 13:35:26 +0200

    Sign-in SensCritique using login details

---
+++ rateItSeven/senscritique.py
@@ -0,0 +1,83 @@
+#   === This file is part of RateItSeven ===
+#
+#   Copyright 2015, Rémi Benoit <r3m1.benoit@gmail.com>
+#
+#   RateItSeven is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   RateItSeven is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with RateItSeven. If not, see <http://www.gnu.org/licenses/>.
+#
+
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver import PhantomJS
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+
+
+LINUX_USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36"
+SC_HOME_PAGE = 'http://senscritique.com'
+
+
+class SensCritique(object):
+
+    '''
+    Interact with SensCritique website
+    '''
+
+    def __init__(self, login, password, userAgent=LINUX_USER_AGENT, homePage=SC_HOME_PAGE):
+        '''
+        Constructor
+
+        :param login:
+        :param password:
+        '''
+
+        self.login = login
+        self.password = password
+
+        dcap = dict(DesiredCapabilities.PHANTOMJS)
+        dcap["phantomjs.page.settings.userAgent"] = (
+            userAgent
+        )
+        self.driver = PhantomJS(desired_capabilities=dcap)
+        self.driver.get(homePage)
+
+    def sign_in(self):
+        '''
+        Sign-in to SensCritique using the given login details
+
+        :rtype: bool
+        :Return: true if login succeeded, false otherwise
+        '''
+
+        loginForm = self.driver.find_element_by_xpath('//*[@id="wrap"]/header/div[1]/div/div/div/div')
+        hover = ActionChains(self.driver).move_to_element(loginForm)
+        hover.perform()
+
+        loginField = self.driver.find_element_by_xpath('//*[@id="wrap"]/header/div[1]/div/div/div/div/form/input[1]')
+        passwordField = self.driver.find_element_by_xpath('//*[@id="wrap"]/header/div[1]/div/div/div/div/form/input[2]')
+
+        loginField.send_keys(self.login)
+        passwordField.send_keys(self.password)
+
+        submit = self.driver.find_element_by_xpath('//*[@id="wrap"]/header/div[1]/div/div/div/div/form/fieldset/input')
+        submit.click()
+
+        try:
+            currentUser = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="wrap"]/header/div[1]/div/div/div/a[3]')))
+            return True
+        except TimeoutException:
+            # TODO : test if loginError is present and log the message
+            return False
+
---
+++ tests/senscritiquetest.py
@@ -0,0 +1,42 @@
+#   === This file is part of RateItSeven ===
+#
+#   Copyright 2015, Rémi Benoit <r3m1.benoit@gmail.com>
+#
+#   RateItSeven is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   RateItSeven is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with RateItSeven. If not, see <http://www.gnu.org/licenses/>.
+#
+
+import unittest
+from rateItSeven.senscritique import SensCritique
+
+
+class TestSensCritique(unittest.TestCase):
+
+    def test_shouldChangeMyUserAgent(self):
+        expectedUserAgent = "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C)"
+        sc = SensCritique("", "", expectedUserAgent, "http://gs.statcounter.com/detect")
+
+        userAgent = sc.driver.find_element_by_xpath('//*[@id="content-inner"]/div/p[1]/em').get_attribute("innerHTML")
+        self.assertEqual(expectedUserAgent, userAgent, "User agent are not equals")
+
+    def test_shouldFailToLogin(self):
+        sc = SensCritique("badlogin", "badpassword")
+        self.assertFalse(sc.sign_in())
+
+    def test_shouldSuccessLogin(self):
+        sc = SensCritique("legalizme@gmail.com", "12345")
+        self.assertTrue(sc.sign_in())
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
