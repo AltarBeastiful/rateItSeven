ffd828d2dcb2ff5c51ba6e8c301e4e11646114c7
Rémi Benoit <r3m1.benoit@gmail.com>
2015-04-24 13:33:03 +0200

    Don't rebuild the WebDriver between integration tests
    Regrouped the tests into a monolithic test to ensure execution order,
    also because we introduced dependency between tests.

--- tests/senscritiquetest.py
+++ tests/senscritiquetest.py
@@ -22,31 +22,43 @@ from rateItSeven.senscritique import SensCritique
 
 class TestSensCritique(unittest.TestCase):
 
-    def corretLogin(self):
-        return SensCritique("legalizme@gmail.com", "12345")
+    def setupBadLogin(self):
+        self.sc.login = self.badLogin
+
+    def setupCorretLogin(self):
+        self.sc.login = self.goodLogin
 
     def test_shouldChangeMyUserAgent(self):
         expectedUserAgent = "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C)"
         sc = SensCritique("", "", expectedUserAgent, "http://gs.statcounter.com/detect")
 
-        userAgent = sc.driver.find_element_by_xpath('//*[@id="content-inner"]/div/p[1]/em').get_attribute("innerHTML")
-        self.assertEqual(expectedUserAgent, userAgent, "User agent are not equals")
+    def testSensCritique(self):
+        # Setup
+        self.badLogin = "badLogin"
 
-    def test_shouldFailToLogin(self):
-        sc = SensCritique("badlogin", "badpassword")
-        self.assertFalse(sc.sign_in())
+        self.goodLogin = "legalizme@gmail.com"
+        self.password = "12345"
 
-    def test_shouldSuccessLogin(self):
-        sc = self.corretLogin()
-        self.assertTrue(sc.sign_in())
+        self.sc = SensCritique("", self.password)
 
-    def test_shouldRetrieveListFromId(self):
-        sc = self.corretLogin()
-        sc.sign_in()
+        # Scenarios
 
+        self.shouldFailToLogin()
+        self.shouldSuccessLogin()
+        self.shouldRetrieveListFromId()
+
+    def shouldFailToLogin(self):
+        self.setupBadLogin()
+        self.assertFalse(self.sc.sign_in())
+
+    def shouldSuccessLogin(self):
+        self.setupCorretLogin()
+        self.assertTrue(self.sc.sign_in())
+
+    def shouldRetrieveListFromId(self):
         listId = "857267"
         listTitle = "Une liste"
-        myList = sc.retrieveListById(listId)
+        myList = self.sc.retrieveListById(listId)
 
         self.assertEqual(listId, myList.id())
         self.assertEqual(listTitle, myList.title())
