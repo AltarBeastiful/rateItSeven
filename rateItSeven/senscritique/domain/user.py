3ee2f051a476750c75a18743a88380a1e4b6583c
oloaP <paolo.devathaire@gmail.com>
2017-02-27 11:19:06 +0100

    Add Missing User domain class

---
+++ rateItSeven/senscritique/domain/user.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#   === This file is part of RateItSeven ===
+#
+#   Copyright 2015, Rémi Benoit <r3m1.benoit@gmail.com>
+#   Copyright 2015, Paolo de Vathaire <paolo.devathaire@gmail.com>
+#
+#   RateItSeven is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   RateItSeven is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with RateItSeven. If not, see <http://www.gnu.org/licenses/>.
+#
+from synthetic import synthesize_constructor
+from synthetic import synthesize_property
+
+
+@synthesize_constructor()
+@synthesize_property('session_cookies')
+@synthesize_property('email')
+@synthesize_property('password')
+class User(object):
+    def __init__(self):
+        pass
