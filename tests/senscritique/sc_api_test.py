a76323b7f4e2cf75843d3c310d41b717333d7600
Rémi Benoit <r3m1.benoit@gmail.com>
2017-03-01 23:56:08 +0100

    Refactor some list tests
    Remove list creation in "find list on multiple page".
    Remove duplicate list creation in setUp.
    Cleanup created list in "find one list" test.

--- tests/senscritique/sc_api_test.py
+++ tests/senscritique/sc_api_test.py
@@ -31,18 +31,6 @@ from tests.lib.test_case import RateItSevenTestCase
 class TestLoginRequest(RateItSevenTestCase):
     LIST_NODES_XPATH = '//div[@data-rel="lists-content"]/ul/li'
 
-    def setUp(self):
-        self.login = "legalizme@gmail.com"
-        self.password = "12345"
-        user = self.authentified_user()
-        listsrv = ListService(user=user)
-        sc_list = listsrv.create_list("myTestList_"+str(datetime.datetime.now()), ListType.MOVIE)
-        self.list_id = sc_list.compute_list_id()
-
-    def tearDown(self):
-        # TODO Delete list
-        pass
-
     def test_login_success(self):
         user = self.authentified_user()
         self.assertNotEqual(0, len(user.session_cookies))
@@ -53,9 +41,11 @@ class TestLoginRequest(RateItSevenTestCase):
             response = AuthService().do_login(u"alogin", "badpassword")
 
     def test_add_movie(self):
-        response = self.authentified_user()
-        listsrv = ListService(response)
-        response = listsrv.add_movie(self.list_id, "11267022", "Un film porno ?")
+        service = ListService(user=self.authentified_user())
+        sc_list = service.create_list("myList_"+str(datetime.datetime.now()), ListType.MOVIE)
+
+        response = service.add_movie(sc_list.compute_list_id(), "11267022", "Un film porno ?")
+
         self.assertIsNotNone(response)
 
     def test_create_list(self):
@@ -77,24 +67,25 @@ class TestLoginRequest(RateItSevenTestCase):
         self.assertEqual("https://www.senscritique.com/sc2/invite-san/listes/all/films/titre/page-1.ajax", url)
 
     def test_find_list_find_some(self):
-        user = self.authentified_user()
-        listsrv = ListService(user=user)
-        listsrv.create_list("find_"+str(datetime.datetime.now()), ListType.MOVIE)
-        listsrv.create_list("find_"+str(datetime.datetime.now()), ListType.MOVIE)
-        # wait 2sec so SensCritique can aknowledge newly created lists
-        time.sleep(2)
-        lists = listsrv.find_list("find_", ListType.MOVIE)
-        self.assertGreater(len(lists), 1)
+        listsrv = ListService(user=self.authentified_user())
 
-    def test_find_list_find_one(self):
-        user = self.authentified_user()
-        listsrv = ListService(user=user)
+        lists = listsrv.find_list("find_", ListType.MOVIE)
+        self.assertGreater(len(lists), 12, "Check if there is still more than 12 list with name like 'find_xxx', if not please create them again.")
+
+    def test_find_list_find_created_one(self):
+        service = ListService(user=self.authentified_user())
+
         unique_name = str(datetime.datetime.now())
-        listsrv.create_list(unique_name, ListType.MOVIE)
+        service.create_list(unique_name, ListType.MOVIE)
         # wait 2sec so SensCritique can aknowledge newly created lists
-        time.sleep(2)
-        lists = listsrv.find_list(unique_name, ListType.MOVIE)
-        self.assertEqual(len(lists), 1)
+        time.sleep(1)
+
+        lists = service.find_list(unique_name, ListType.MOVIE)
+        self.assertEqual(1, len(lists))
+
+        # @todo Helper to create and cleanup a list?
+        # Clean up created list
+        service.delete_list(list=lists[0])
 
     def test_find_list_find_none(self):
         user = self.authentified_user()
