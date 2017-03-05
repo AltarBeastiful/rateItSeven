65959050a2eda8ea9a0435f6dfc87e96b5fef718
Rémi Benoit <r3m1.benoit@gmail.com>
2017-03-05 21:41:49 +0100

    Add LocalCollectionStore storing Piece in a local database

---
+++ rateItSeven/scan/local_collection_store.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#   === This file is part of RateItSeven ===
+#
+#   Copyright 2017, Rémi Benoit <r3m1.benoit@gmail.com>
+#   Copyright 2017, Paolo de Vathaire <paolo.devathaire@gmail.com>
+#
+#   RateItSeven is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   RateItSeven is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with RateItSeven. If not, see <http://www.gnu.org/licenses/>.
+#
+from tinydb import JSONStorage
+from tinydb import TinyDB, Query
+from contracts import new_contract
+
+from rateItSeven.scan.piece import Piece
+
+new_contract('Piece', Piece)
+
+
+class LocalCollectionStore(object):
+    LOCAL_DB_PATH = "mydb.json"
+
+    def __init__(self):
+        if TinyDB.DEFAULT_STORAGE == JSONStorage:
+            self._db = TinyDB(path=self.LOCAL_DB_PATH)
+        else:
+            self._db = TinyDB()
+
+    def add(self, piece):
+        """
+        :param Piece piece:
+        :return:
+        """
+        self._db.insert(piece.to_dict())
+
+    def piece_list(self):
+        """
+        :rtype: list(Piece)
+        """
+
+        return [Piece.from_dict(piece_dict) for piece_dict in self._db.all()]
+
---
+++ tests/scan/test_local_collection_store.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#   === This file is part of RateItSeven ===
+#
+#   Copyright 2017, Rémi Benoit <r3m1.benoit@gmail.com>
+#   Copyright 2017, Paolo de Vathaire <paolo.devathaire@gmail.com>
+#
+#   RateItSeven is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   RateItSeven is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with RateItSeven. If not, see <http://www.gnu.org/licenses/>.
+#
+from tinydb import TinyDB
+from tinydb.storages import MemoryStorage
+
+from rateItSeven.scan.local_collection_store import LocalCollectionStore
+from rateItSeven.scan.piece import Piece
+from tests.lib.test_case import RateItSevenTestCase
+
+
+class TestLocalCollectionStore(RateItSevenTestCase):
+
+    def setUp(self):
+        TinyDB.DEFAULT_STORAGE = MemoryStorage
+
+    def test_should_list_all_saved_piece(self):
+        collection = LocalCollectionStore()
+
+        piece_1 = Piece(path="some/path/1")
+        piece_2 = Piece(path="some/path/2")
+
+        collection.add(piece=piece_1)
+        collection.add(piece=piece_2)
+
+        self.assertEqual([piece_1, piece_2], collection.piece_list())
