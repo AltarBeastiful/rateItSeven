16dd3ac9ea34a130e203ef3a79c448f596a5d658
oloaP <paolo.devathaire@gmail.com>
2017-03-14 11:32:26 +0100

    Factorize list service test initialization and share list between tests when possible

--- tests/senscritique/test_list_service.py
+++ tests/senscritique/test_list_service.py
@@ -20,7 +20,6 @@
 #   along with RateItSeven. If not, see <http://www.gnu.org/licenses/>.
 #
 import datetime
-import time
 
 from rateItSeven.senscritique.domain.product import ProductType, Product
 from rateItSeven.senscritique.domain.sc_list import ListType, ScList
@@ -30,106 +29,79 @@ from tests.lib.test_case import RateItSevenTestCase
 
 class TestListService(RateItSevenTestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.service = ListService(user=RateItSevenTestCase.authentified_user())
+        cls.unique_list = cls.service.create_list(str(datetime.datetime.now()), ListType.MOVIE)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.service.delete_list(cls.unique_list)
+
     def test_create_list(self):
-        user = self.authentified_user()
-        listsrv = ListService(user=user)
-        sc_list = listsrv.create_list("myList_"+str(datetime.datetime.now()), ListType.MOVIE)
+        sc_list = self.service.create_list("myList_" + str(datetime.datetime.now()), ListType.MOVIE)
         self.assertIsNotNone(sc_list.path)
 
     def test_build_list_search_url_no_type(self):
-        user = self.authentified_user()
-        listsrv = ListService(user=user)
-        url = listsrv._build_list_search_url()
+        url = self.service._build_list_search_url()
         self.assertEqual("https://www.senscritique.com/sc2/invite-san/listes/all/all/titre/page-1.ajax", url)
 
     def test_build_list_search_url_movie(self):
-        user = self.authentified_user()
-        listsrv = ListService(user=user)
-        url = listsrv._build_list_search_url(list_type=ListType.MOVIE)
+        url = self.service._build_list_search_url(list_type=ListType.MOVIE)
         self.assertEqual("https://www.senscritique.com/sc2/invite-san/listes/all/films/titre/page-1.ajax", url)
 
     def test_find_list_find_some(self):
-        listsrv = ListService(user=self.authentified_user())
-
-        lists = listsrv.find_list("find_", ListType.MOVIE)
+        lists = self.service.find_list("find_", ListType.MOVIE)
         self.assertCountGreater(lists, 12, "Check if there is still more than 12 list with name like 'find_xxx', if not please create them again.")
 
     def test_find_list_find_created_one(self):
-        service = ListService(user=self.authentified_user())
-
-        unique_name = str(datetime.datetime.now())
-        service.create_list(unique_name, ListType.MOVIE)
-        # wait 2sec so SensCritique can aknowledge newly created lists
-        time.sleep(2)
-
-        lists = list(service.find_list(unique_name, ListType.MOVIE))
+        lists = list(self.service.find_list(self.unique_list.name, self.unique_list.type))
         self.assertEqual(1, len(lists))
 
-        # @todo Helper to create and cleanup a list?
-        # Clean up created list
-        service.delete_list(list=lists[0])
-
     def test_find_list_find_none(self):
-        user = self.authentified_user()
-        listsrv = ListService(user=user)
-        lists = listsrv.find_list("YoucannotFindme0987654321123", ListType.MOVIE)
+        lists = self.service.find_list("YoucannotFindme0987654321123", ListType.MOVIE)
         self.assertIsNone(next(lists, None))
 
     def test_delete_list(self):
-        service = ListService(user=self.authentified_user())
         unique_name = "Tobedeleted%s" % (str(datetime.datetime.now()))
-        sc_list = service.create_list(unique_name, ListType.MOVIE)
+        sc_list = self.service.create_list(unique_name, ListType.MOVIE)
 
-        delete_response = service.delete_list(list=sc_list)
+        delete_response = self.service.delete_list(list=sc_list)
 
         self.assertTrue(delete_response)
-        self.assertEqual([], list(service.find_list(title=unique_name, list_type=ListType.MOVIE)))
+        self.assertEqual([], list(self.service.find_list(title=unique_name, list_type=ListType.MOVIE)))
 
     def test_delete_unknown_list_returns_true(self):
-        service = ListService(user=self.authentified_user())
         sc_list = ScList(type=ListType.MOVIE, name="unknown", path="list/unknown/12345")
-
-        self.assertTrue(service.delete_list(list=sc_list))
+        self.assertTrue(self.service.delete_list(list=sc_list))
 
     def test_add_movie(self):
-        service = ListService(user=self.authentified_user())
-        sc_list = service.create_list("myList_"+str(datetime.datetime.now()), ListType.MOVIE)
-
-        response = service.add_movie(sc_list.compute_list_id(), "11267022", "Un film porno ?")
-
+        response = self.service.add_movie(self.unique_list.compute_list_id(), "11267022", "Un film porno ?")
         self.assertIsNotNone(response)
 
     def test_remove_movie(self):
-        service = ListService(user=self.authentified_user())
-        sc_list = service.create_list("myList_"+str(datetime.datetime.now()), ListType.MOVIE)
-        item_id = service.add_movie(sc_list.compute_list_id(), "11267022")
-        removed = service.remove_movie(sc_list.compute_list_id(), item_id)
+        sc_list = self.service.create_list("myList_" + str(datetime.datetime.now()), ListType.MOVIE)
+        item_id = self.service.add_movie(sc_list.compute_list_id(), "11267022")
+        removed = self.service.remove_movie(sc_list.compute_list_id(), item_id)
         self.assertTrue(removed)
 
     def test_add_episode_serie_not_yet_added(self):
-        service = ListService(user=self.authentified_user())
-        sc_list = service.create_list("myList_"+str(datetime.datetime.now()), ListType.MOVIE)
-
-        response = service.add_episode(sclist=sc_list, product_id="444509", description="S04E01")
-
+        response = self.service.add_episode(sclist=self.unique_list, product_id="444509", description="S04E01")
         self.assertIsNotNone(response)
 
     def test_add_episode_serie_already_added(self):
-        service = ListService(user=self.authentified_user())
-        sc_list = service.create_list("myList_"+str(datetime.datetime.now()), ListType.MOVIE)
+        self.service.add_episode(sclist=self.unique_list, product_id="444509", description="S04E01")
+        self.service.add_episode(sclist=self.unique_list, product_id="444509", description="S04E02")
 
-        service.add_episode(sclist=sc_list, product_id="444509", description="S04E01")
-        service.add_episode(sclist=sc_list, product_id="444509", description="S04E02")
-
-        list_item = service.find_list_item(sclist=sc_list, product_id="444509")
+        list_item = self.service.find_list_item(sclist=self.unique_list, product_id="444509")
         self.assertIn("S04E01",list_item.description)
         self.assertIn("S04E02",list_item.description)
 
     def test_should_find_all_list_items(self):
-        service = ListService(user=self.authentified_user())
-        sc_list = next(service.find_list(title="DONOTCHANGE_list_with_items", list_type=ListType.MOVIE))
+        sc_list = next(self.service.find_list(title="DONOTCHANGE_list_with_items", list_type=ListType.MOVIE))
 
-        item_list = list(service.list_item_list(sc_list=sc_list))
+        item_list = list(self.service.list_item_list(sc_list=sc_list))
 
         self.assertEqual(Product(id="373249", title="Izo", type=ProductType.MOVIE), item_list[0].product)
         self.assertEqual("a description", item_list[0].description)
@@ -138,8 +110,7 @@ class TestListService(RateItSevenTestCase):
         self.assertCountGreater(item_list, 30)
 
     def test_find_list_item_on_second_page(self):
-        service = ListService(user=self.authentified_user())
-        sc_list = next(service.find_list(title='DONOTCHANGE_list_with_items', list_type=ListType.MOVIE))
+        sc_list = next(self.service.find_list(title='DONOTCHANGE_list_with_items', list_type=ListType.MOVIE))
 
-        list_item = service.find_list_item(sclist=sc_list, product_id='467126')
+        list_item = self.service.find_list_item(sclist=sc_list, product_id='467126')
         self.assertEqual(Product(id='467126', title='Dead Man', type=ProductType.MOVIE), list_item.product)
